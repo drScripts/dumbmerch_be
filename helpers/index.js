90ea93ce756e9df24c40c5f99e60cee55be7bc9a
Nathanael <nathanael.vd@gmail.com>
2022-04-22 02:46:07 +0700

    update

--- helpers/index.js
+++ helpers/index.js
@@ -955,7 +955,7 @@ const sendMail = async (user_email, invoice_html) => {
   await transporter.sendMail({
     subject: "DumbMerch Invoice",
     to: user_email,
-    from: "DumbMerch Team",
+    from: "noreply.mailers.com",
     subject: "Your current transaction invoice",
     html: "<html><body>test</body></html>",
     text: "https://dumbmerch-drscripts.netlify.app",
