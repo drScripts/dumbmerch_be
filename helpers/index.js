2e48f08208740e16c840eea6f2a884ae4f5116bb
Nathanael <nathanael.vd@gmail.com>
2022-04-22 02:59:12 +0700

    update

--- helpers/index.js
+++ helpers/index.js
@@ -940,7 +940,7 @@ const sendMailTest = async (user_email, invoice_html) => {
 };
 
 const sendMail = async (user_email, invoice_html) => {
-  console.log("LOGGG", sendinBluePass, sendinBlueUser);
+  console.log("LOGGG", sendinBluePass, sendinBlueUser, user_email);
   /**
    * With Sendin Blue
    */
